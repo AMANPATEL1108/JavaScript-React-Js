cea081c26fa15b94e24e21bdcaa238f464676587
AMANPATEL <amanpatel8844382@gmail.com>
2024-12-24 14:26:24 +0530

    first commit

---
+++ JavaScript/01/first.js
@@ -0,0 +1,65 @@
+// console.log("Aman Patel");
+
+// name = "aman patel Ambalal";
+// age = 21;
+// x = null;
+// y = undefined;
+// console.log(name);
+// console.log(age);
+// console.log(x);
+// console.log(y);
+// name = "Aman is Here ";
+// isFollow = true;
+// console.log(isFollow);
+// console.log(name);
+
+// Fullname = "Aman";
+// fullname = "Patel";
+
+// console.log(Fullname);
+// console.log(fullname);
+
+// Console = "Animation";
+// console.log(Console);
+
+// var a = 10;
+// var a = 20;
+// console.log(a); //20
+
+// let name = "Aman Patel";
+// name = "Patel Aman"; //Print
+// console.log(name);
+
+// const age = 5;
+// // age = 27; //Error Not Use or Re Declare
+// console.log(age);
+
+// const student = {
+//   age: 20,
+//   name: "Bharat",
+//   cgpa: 8.2,
+//   isPass: true,
+// };
+
+// student["age"] = student["age"] + 12;
+// // student.age = student.age + 12;
+
+// console.log(student.age);
+
+// const Pen = {
+//   name: "Parker jotter",
+//   rating: 5,
+//   discount: 5,
+//   price: 270,
+// };
+
+// console.log(Pen);
+
+const Profile = {
+  userName: "Aman PAtel",
+  isFollow: false,
+  followers: 123,
+  follwing: 75,
+};
+
+console.log(Profile);
